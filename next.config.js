0a7111aa826c29db7b37344db5a3e060a63ce127
Vikram Belde <theecomway.team@gmail.com>
2025-10-03 16:50:16 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  experimental: {
-    appDir: false
-  },
   env: {
     CUSTOM_KEY: process.env.CUSTOM_KEY,
   },
